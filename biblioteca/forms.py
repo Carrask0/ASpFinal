a27b602daa56522bdc0cb63e6d8f64354b03a819
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2024-05-10 13:39:41 +0200

    workign. will add css

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -1,8 +1,28 @@
 from django import forms
-from .models import Libro
+from .models import Libro, Biblioteca
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
         fields = '__all__'  # Include all fields from the Libro model
+
+# forms.py
+from django import forms
+from .models import Biblioteca
+
+class BibliotecaForm(forms.ModelForm):
+    # Define custom form fields for hours and date
+    horario_apertura = forms.TimeField(input_formats=['%H:%M'], widget=forms.TimeInput(attrs={'type': 'time'}))
+    horario_cierre = forms.TimeField(input_formats=['%H:%M'], widget=forms.TimeInput(attrs={'type': 'time'}))
+    fecha_fundacion = forms.DateField(input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Biblioteca
+        fields = ['nombre', 'direccion', 'ciudad', 'horario_apertura', 'horario_cierre', 'fecha_fundacion', 'normas']
+
+    def clean_direccion(self):
+        direccion = self.cleaned_data.get('direccion')
+        if not direccion.endswith('0') and not direccion.endswith('1') and not direccion.endswith('2') and not direccion.endswith('3') and not direccion.endswith('4') and not direccion.endswith('5') and not direccion.endswith('6') and not direccion.endswith('7') and not direccion.endswith('8') and not direccion.endswith('9'):
+            raise forms.ValidationError("La dirección debe contener un número al final.")
+        return direccion
 
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Biblioteca, Libro
+
 from faker import Faker
 import random
-from .forms import LibroForm
+
+from .forms import LibroForm, BibliotecaForm
+from .models import Biblioteca, Libro
+
 
 
 # Create your views here.
@@ -58,22 +61,24 @@ def lista_bibliotecas(request):
     # Retrieve all libraries by default
     bibliotecas = Biblioteca.objects.all()
 
-    # Check if the request is a POST request and process form submissions
+    # Initialize nombre and ciudad to None
+    nombre = None
+    ciudad = None
+
+    # Check if the request is POST request and process form submissions
     if request.method == 'POST':
         # Check if the form for searching by library name is submitted
-        if 'nombre' in request.POST:
-            nombre = request.POST['nombre']
-            # Filter libraries by name
-            bibliotecas = Biblioteca.objects.filter(nombre__icontains=nombre)
+        if 'nombre' in request.POST or 'ciudad' in request.POST:
+            nombre = request.POST.get('nombre', '')
+            ciudad = request.POST.get('ciudad', '')
 
-        # Check if the form for searching by city is submitted
-        elif 'ciudad' in request.POST:
-            ciudad = request.POST['ciudad']
-            # Filter libraries by city
-            bibliotecas = Biblioteca.objects.filter(ciudad__icontains=ciudad)
+            # Filter libraries by name and city
+            bibliotecas = bibliotecas.filter(nombre__icontains=nombre, ciudad__icontains=ciudad)
 
-    # Render the template with the filtered or all libraries
-    return render(request, "lista_bibliotecas.html", {"bibliotecas": bibliotecas})
+
+    # Render the template with the filtered or all libraries, and pass nombre and ciudad
+    return render(request, "lista_bibliotecas.html", {"bibliotecas": bibliotecas, "nombre": nombre, "ciudad": ciudad})
+
 
 
 def lista_libros(request):
@@ -89,99 +94,17 @@ def lista_libros(request):
     """
     libros = Libro.objects.all()
 
+    # Initialize titulo to None
+    titulo = None
+
     # Check if the request is a POST request and process form submissions
     if request.method == 'POST' and 'titulo' in request.POST:
         titulo = request.POST['titulo']
         # Filter libros by titulo
         libros = Libro.objects.filter(titulo__icontains=titulo)
 
-    return render(request, "lista_libros.html", {"libros": libros})
+    return render(request, "lista_libros.html", {"libros": libros, "titulo": titulo})
 
-
-def lista_libros_biblioteca(request, biblioteca_id):
-    """
-    Retrieves a list of books belonging to a specific library from the database and renders them in a template.
-
-    Parameters:
-    - request: An HttpRequest object representing the request made by the user.
-    - biblioteca_id: An integer representing the ID of the library.
-
-    Returns:
-    - An HttpResponse object representing the rendered HTML page displaying the books belonging to the specified library.
-    """
-    biblioteca = Biblioteca.objects.get(pk=biblioteca_id)
-    libros = Libro.objects.filter(biblioteca=biblioteca)
-    return render(request, "lista_libros.html", {"libros": libros})
-
-
-def buscar_libro_titulo_biblioteca(request, titulo, biblioteca_id):
-    """
-    Searches for books by title within a specific library and renders them in a template.
-
-    Parameters:
-    - request: An HttpRequest object representing the request made by the user.
-    - titulo: A string representing the title of the book to search for.
-    - biblioteca_id: An integer representing the ID of the library.
-
-    Returns:
-    - An HttpResponse object representing the rendered HTML page displaying the books matching the search criteria within the specified library.
-    """
-    biblioteca = Biblioteca.objects.get(pk=biblioteca_id)
-    libros = Libro.objects.filter(titulo__icontains=titulo, biblioteca=biblioteca)
-    return render(request, "lista_libros.html", {"libros": libros})
-
-
-def buscar_libro_autor_biblioteca(request, autor, biblioteca_id):
-    """
-    Searches for books by author within a specific library and renders them in a template.
-
-    Parameters:
-    - request: An HttpRequest object representing the request made by the user.
-    - autor: A string representing the author of the book to search for.
-    - biblioteca_id: An integer representing the ID of the library.
-
-    Returns:
-    - An HttpResponse object representing the rendered HTML page displaying the books written by the specified author within the specified library.
-    """
-    biblioteca = Biblioteca.objects.get(pk=biblioteca_id)
-    libros = Libro.objects.filter(autor__icontains=autor, biblioteca=biblioteca)
-    return render(request, "lista_libros.html", {"libros": libros})
-
-
-def buscar_libro_editorial_biblioteca(request, editorial, biblioteca_id):
-    """
-    Searches for books by editorial within a specific library and renders them in a template.
-
-    Parameters:
-    - request: An HttpRequest object representing the request made by the user.
-    - editorial: A string representing the editorial of the book to search for.
-    - biblioteca_id: An integer representing the ID of the library.
-
-    Returns:
-    - An HttpResponse object representing the rendered HTML page displaying the books published by the specified editorial within the specified library.
-    """
-    biblioteca = Biblioteca.objects.get(pk=biblioteca_id)
-    libros = Libro.objects.filter(editorial__icontains=editorial, biblioteca=biblioteca)
-    return render(request, "lista_libros.html", {"libros": libros})
-
-
-def buscar_libro_titulo(request, titulo):
-    """
-    Searches for books by title across all libraries and renders them in a template.
-
-    Parameters:
-    - request: An HttpRequest object representing the request made by the user.
-    - titulo: A string representing the title of the book to search for.
-
-    Returns:
-    - An HttpResponse object representing the rendered HTML page displaying the books matching the search criteria.
-    """
-    libros = Libro.objects.filter(titulo__icontains=titulo)
-    return render(request, "lista_libros.html", {"libros": libros})
-
-
-from django.shortcuts import render, get_object_or_404
-from .models import Biblioteca, Libro
 
 def biblioteca_detail(request, biblioteca_id):
     """
@@ -201,6 +124,11 @@ def biblioteca_detail(request, biblioteca_id):
     # Retrieve all libros associated with the biblioteca
     libros = biblioteca.libro_set.all()
 
+    # Initialize search criteria to empty strings
+    titulo = None
+    autor = None
+    editorial = None
+
     # Check if the request is a POST request and process form submissions
     if request.method == 'POST':
         # Check if the form for searching libros is submitted
@@ -212,7 +140,7 @@ def biblioteca_detail(request, biblioteca_id):
             # Filter libros based on search criteria
             libros = libros.filter(titulo__icontains=titulo, autor__icontains=autor, editorial__icontains=editorial)
 
-    return render(request, "biblioteca_detail.html", {"biblioteca": biblioteca, "libros": libros})
+    return render(request, "biblioteca_detail.html", {"biblioteca": biblioteca, "libros": libros, "titulo": titulo, "autor": autor, "editorial": editorial})
 
 
 def libro_detail(request, libro_id):
@@ -240,7 +168,6 @@ def delete_libro(request, libro_id):
     Returns:
     - An HttpResponse object indicating the result of the operation.
     """
-    print("Deleting libro")
     libro = Libro.objects.get(pk=libro_id)
     libro.delete()
 
@@ -294,6 +221,47 @@ def form_create_libro(request):
         form = LibroForm()
         bibliotecas = Biblioteca.objects.all()
     return render(request, "form_libro.html", {"bibliotecas": bibliotecas})
+
+
+def form_create_biblioteca(request):
+    if request.method == 'POST':
+        form = BibliotecaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/bibliotecas/")
+    else:
+        form = BibliotecaForm()
+    return render(request, 'form_biblioteca.html', {'form': form})
+
+def form_edit_biblioteca(request, biblioteca_id):
+    biblioteca = Biblioteca.objects.get(pk=biblioteca_id)
+    if request.method == 'POST':
+        form = BibliotecaForm(request.POST, instance=biblioteca)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/bibliotecas/")
+    else:
+        form = BibliotecaForm(instance=biblioteca)
+    return render(request, 'form_biblioteca.html', {'form': form, 'biblioteca': biblioteca})
+
+
+def delete_biblioteca(request, biblioteca_id):
+    """
+    Deletes a specific library from the database.
+
+    Parameters:
+    - request: An HttpRequest object representing the request made by the user.
+    - biblioteca_id: An integer representing the ID of the library to delete.
+
+    Returns:
+    - An HttpResponse object indicating the result of the operation.
+    """
+    print("Deleting biblioteca")
+    biblioteca = Biblioteca.objects.get(pk=biblioteca_id)
+    biblioteca.delete()
+
+    # Redirect to lista_bibliotecas view
+    return HttpResponseRedirect("/bibliotecas/")
 
         
 
